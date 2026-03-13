17d62f978a00701f2e78d388cf0f6f66320eaaf9
Arissj4 <eris1376@gmail.com>
2026-03-13 10:03:52 +0100

    [ADD] Implemented new packages, new pages, basic design

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,31 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
+import "./index.scss"
 import UserExercisePrograms from './components/UserExercisePrograms'
+import Home from './components/Home'
+import Login from './components/Login'
+import Sidebar from './components/Sidebar'
+
 
 function App() {
 
+
+  function handleNavigate(route: string): void{
+    console.log(route);
+  }
+  
   return (
     <>
-      <UserExercisePrograms programs = {["Program 1", "Program 2"]}/>
+      <div id="wrapper" className='rounded-xl'>
+        <Sidebar userFullName='Soheil Jamshidi' handleNavigate={handleNavigate}/>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/user-exercise-programs" element={<UserExercisePrograms programs={['Program 1', 'Program 2', 'Program 3']} />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   )
 }
---
+++ front-end/src/components/Sidebar.tsx
@@ -0,0 +1,65 @@
+import React, { type JSX } from 'react'
+import '../style/Sidebar.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faCircleUser } from '@fortawesome/free-regular-svg-icons'
+import { faBars, faListOl } from "@fortawesome/free-solid-svg-icons"
+import type { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { useNavigate } from 'react-router'
+
+
+type Props = {
+  userImage?: string | null,
+  userFullName: string;
+  handleNavigate: (route: string) => void;
+}
+
+const Sidebar = (props: Props) => {
+  function sidebarElementCreator(logo: IconProp, title: string, route: string): JSX.Element {
+    return (
+      <li onClick={() => props.handleNavigate(route)}>
+        <FontAwesomeIcon icon={logo} />
+        {title}
+      </li>
+    )
+  }
+
+  const sidebarElementsList = [
+    sidebarElementCreator(faHouse, 'Home', '/'),
+    sidebarElementCreator(faListOl, 'My Programs', '/user-exercise-programs'),
+  ]
+
+  
+  return (
+    <div id="sidebar" className='border-r'>
+      <ul id="sidebar-list">
+        <div>
+          <div>
+            {props.userImage ? (
+              <img src='{props.userImage}'/>
+            ) : (
+              <FontAwesomeIcon icon={faCircleUser} />
+            )}
+          </div>
+
+          {/* <div>
+            {props.userFullName ? props.userFullName : 'Default User'}
+          </div> */}
+
+          <div>
+            <FontAwesomeIcon icon={faBars} />
+          </div>
+        </div>
+
+        <div>
+          {sidebarElementsList}
+        </div>
+
+        <div>
+
+        </div>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
