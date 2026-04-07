322d307f34d5c0085e520921c4ec5c18b43c766f
Arissj4 <eris1376@gmail.com>
2026-04-07 18:53:38 +0200

    [FIX] Removing Djang

--- back-end/user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import hello, health
-
-urlpatterns = [
-    path("hello/", hello),
-    path("health/", health),
-]
--- front-end/src/components/CreateWorkout.tsx
+++ front-end/src/components/CreateWorkout.tsx
@@ -6,7 +6,9 @@ function CreateWorkout(props: Props) {
   return (
     <div id='create-workout' className='flex-auto h-full p-6 flex-col justify-center w-[70%]'>
       <div>
-
+        <span className='text-[24px] font-[1000] text-orange-500 block w-full h-fit text-center'>
+          Create Workout Plan
+        </span>
       </div>
     </div>
   )
