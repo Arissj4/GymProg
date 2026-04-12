91c63df82d2e235c9180b8d6e09df1abc809df1a
Arissj4 <eris1376@gmail.com>
2026-04-12 21:43:50 +0200

    MYPROG [ADD] Implemented input elements for login page

--- front-end/src/pages/Login.tsx
+++ front-end/src/pages/Login.tsx
@@ -38,9 +38,44 @@ function Login ( props: Props) {
 
   return (
     <div id='login' className='flex-auto h-full p-6 flex-col justify-center w-[70%]'>
-      <div className='flex h-max'>
-        <ButtonComponent model={loginButton}/>
-        <ButtonComponent model={registerButton}/>
+      <div className='flex-auto justify-center h-max'>
+        <div className='mb-4'>
+
+          <form className='flex flex-col items-center justify-center'>
+            <div className='flex items-center justify-between w-5/10 m-1'>
+              <label className='h-fit w-2/10 items-center' htmlFor='email'>
+                Email:
+              </label>
+
+              <input className='w-7/10 p-1 rounded-md bg-transparent border border-gray-400 hover:border-gray-500 focus:outline-none'
+              type='email'
+              id='name'
+              placeholder='Email' />
+            </div>
+
+            <div className='flex items-center justify-between w-5/10 m-1'>
+              <label className='h-fit w-2/10' htmlFor='password'>
+                Password:
+              </label>
+              <input className='w-7/10 p-1 rounded-md bg-transparent border border-gray-400 hover:border-gray-500 focus:outline-none'
+                type='password'
+                name='password'
+                placeholder='Password' />
+            </div>
+
+            <div className='flex items-center justify-between w-5/10 m-1'>
+              <span>
+                Do not have an account?
+              </span>
+            </div>
+
+          </form>
+        </div>
+
+        <div className='flex justify-center bg-amber-800'>
+          <ButtonComponent model={loginButton}/>
+          <ButtonComponent model={registerButton}/>
+        </div>
       </div>
     </div>
   )
