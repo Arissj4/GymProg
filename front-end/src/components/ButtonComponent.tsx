9afbd64bdf9d67a5286bd9845490e41ae0550f6b
Arissj4 <eris1376@gmail.com>
2026-04-12 10:41:27 +0200

    MYPROG [FIX] Fixed Button component dynamic sizing

--- front-end/src/components/ButtonComponent.tsx
+++ front-end/src/components/ButtonComponent.tsx
@@ -10,10 +10,10 @@ const ButtonComponent = (props: Props) => {
   const getButtonType = () => {
     switch (props.model.type){
       case "orange":
-        return `border p-2 rounded-xl bg-orange-500 text-white hover:bg-orange-600 hover:cursor-pointer w-[${props.model.size}]`
+        return `border p-2 rounded-xl bg-orange-500 text-white hover:bg-orange-600 hover:cursor-pointer`
         break;
       case "white":
-        return `border border-[#d4d4d8] p-2 rounded-xl bg-white text-[#454856] hover:bg-gray-100/70 hover:cursor-pointer w-[${props.model.size}]`
+        return `border border-[#d4d4d8] p-2 rounded-xl bg-white text-[#454856] hover:bg-gray-100/70 hover:cursor-pointer`
         break;
     }
   }
@@ -23,6 +23,7 @@ const ButtonComponent = (props: Props) => {
       <button
         onClick={props.model.clickEvent}
         className={getButtonType()}
+        style={{ width: props.model.size }}
       >
         {props.model.text}
       </button>
